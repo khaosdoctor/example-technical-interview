b86669ea9296efd73b5ecbda486264048abfb917
Lucas Santos <lhs.santoss@gmail.com>
2023-11-12 17:58:39 +0100

    Remove jest
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("ts-jest").JestConfigWithTsJest} */
-module.exports = {
-    verbose: true,
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    silent: false,
-    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-};
